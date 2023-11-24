13c2ea0bd49b6235b22749361a26440c4c03344d
AngouXYZ <137975406+AngouXYZ@users.noreply.github.com>
2023-11-25 03:20:38 +0800

    Update Container.js

--- components/Container.js
+++ components/Container.js
@@ -11,8 +11,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
 
   const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link
   const meta = {
-    //title: BLOG.title,
-    title: "暗号",
+    title: "BLOG.title",
     type: 'website',
     ...customMeta
   }
